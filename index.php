f76674bc3e52b530db84507d6caaf7fd4129e103
Jaime supo <jaimesupo@gmail.com>
2022-09-15 15:40:50 -0500

    funciona

--- index.php
+++ index.php
@@ -4,12 +4,13 @@ use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 use Google\Cloud\Vision\V1\Feature\Type;
 use Google\Cloud\Vision\V1\Likelihood;
 $count=0;
+$url=$_POST["value"];
 $imageAnnotator = new ImageAnnotatorClient(
     [
         'credentials' => 'key.json'
     ]
 );
-$path = "https://img.freepik.com/vector-gratis/conjunto-muebles-hogar_74855-15461.jpg?w=2000";
+$path = $url;
  $image = file_get_contents($path);
  //$image= fopen($_FILES['image']['tmp_name'],'r');
  $response = $imageAnnotator->objectLocalization($image);
