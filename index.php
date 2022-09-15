12f7b342dd2bebb1cedac21d01044adcd3f8bb8d
Jaime supo <jaimesupo@gmail.com>
2022-09-15 15:59:35 -0500

    foto

--- index.php
+++ index.php
@@ -4,13 +4,12 @@ use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 use Google\Cloud\Vision\V1\Feature\Type;
 use Google\Cloud\Vision\V1\Likelihood;
 $count=0;
-$url=$_POST["value"];
 $imageAnnotator = new ImageAnnotatorClient(
     [
         'credentials' => 'key.json'
     ]
 );
-$path = $url;
+$path = "https://img.freepik.com/vector-gratis/conjunto-muebles-hogar_74855-15461.jpg?w=2000";
  $image = file_get_contents($path);
  //$image= fopen($_FILES['image']['tmp_name'],'r');
  $response = $imageAnnotator->objectLocalization($image);
